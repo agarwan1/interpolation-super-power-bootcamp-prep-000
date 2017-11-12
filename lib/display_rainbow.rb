a8af80cef626b38ffa731d0e8dec866849f2dffc
Nisha Agarwal <agarwan1192@gmail.com>
2017-11-12 16:59:17 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -9,5 +9,5 @@ puts "Y: #{colors[2]}"
 puts "G: #{colors[3]}"
 puts "B: #{colors[4]}"
 puts "I: #{colors[5]}"
-puts "Y=V: #{colors[6]}"
+puts "V: #{colors[6]}"
 # Write your #display_rainbow method here
